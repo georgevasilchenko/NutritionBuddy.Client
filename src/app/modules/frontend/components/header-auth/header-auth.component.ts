7737565d0df7573b5f21f64ccbddfb47f18d3fa7
George Vasilchenko <george.vasilchenko@gmail.com>
2017-12-04 23:02:45 +0100

    NB01-6
    User image instead of username and layout for login page.

--- src/app/modules/frontend/components/header-auth/header-auth.component.ts
+++ src/app/modules/frontend/components/header-auth/header-auth.component.ts
@@ -5,6 +5,8 @@ import {LocalStorageService} from '../../services/local-storage.service';
 import {UserRouteIds} from '../../../nutrition-buddy/identity/globals/user-route-ids';
 import {RoutingService} from '../../services/routing.service';
 import {FrontendRouteIds} from '../../globals/frontend-route-ids';
+import {UserRepositoryService} from "../../../nutrition-buddy/identity/services/user-repository.service";
+import {IUser, User} from "../../../nutrition-buddy/identity/models/user.model";
 
 @Component({
   selector: FrontendSelectorsIds.HeaderAuthenticationSelector,
@@ -13,11 +15,13 @@ import {FrontendRouteIds} from '../../globals/frontend-route-ids';
 })
 export class HeaderAuthComponent implements OnInit {
 
-  greeting = 'null';
+  userName = 'null';
   profilePath: string;
   logOutPath: string;
+  user: User;
 
   constructor(private _authService: AuthService,
+              private _userRepositoryService: UserRepositoryService,
               private _localStorageService: LocalStorageService,
               private _routingService: RoutingService) {
   }
@@ -25,10 +29,16 @@ export class HeaderAuthComponent implements OnInit {
   ngOnInit() {
 
     if (this._authService.isAuthenticated()) {
-      this.greeting = this._localStorageService.getUser().userName;
+      this.userName = this._localStorageService.getUser().userName;
+      this.profilePath = this._routingService.getNavigationOfRoute(UserRouteIds.UserEdit) + this._localStorageService.getUser().id;
+      this.logOutPath = this._routingService.getNavigationOfRoute(FrontendRouteIds.Login);
+
+      this._userRepositoryService.getById(this._localStorageService.getUser().id)
+        .then((response) => {
+          const userSpec = <IUser>response;
+          this.user = new User(userSpec);
+        });
     }
-    this.profilePath = this._routingService.getNavigationOfRoute(UserRouteIds.UserEdit) + this._localStorageService.getUser().id;
-    this.logOutPath = this._routingService.getNavigationOfRoute(FrontendRouteIds.Login);
   }
 
   logOut(): void {
