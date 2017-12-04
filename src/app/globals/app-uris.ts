2eedb70fabaa05cadcdbe30c4565d88df96eb43f
George Vasilchenko <george.vasilchenko@gmail.com>
2017-12-04 20:56:36 +0100

    Changed API uri.

--- src/app/globals/app-uris.ts
+++ src/app/globals/app-uris.ts
@@ -1,13 +1,13 @@
 export class AppUris {
   static IsDev = false;
-  static ProdUri = 'http://nutrition-buddy.azurewebsites.net/';
+  static ProdUri = 'http://nutrition-buddy-api.azurewebsites.net/';
   static DevUri = 'http://localhost:5000/';
   static RootUri = AppUris.IsDev ? AppUris.DevUri : AppUris.ProdUri;
 
   // Data
   static ImageDataGet = (uri: string) => {
     return AppUris.RootUri + 'api/data/getimagedata?uri=' + uri;
-  };
+  }
 
   // Product
   static ProductGetAll = AppUris.RootUri + 'api/product/getall';
@@ -17,27 +17,7 @@ export class AppUris {
   static ProductSearch = AppUris.RootUri + 'api/product/search';
   static ProductGeById = (id: number) => {
     return AppUris.RootUri + 'api/product/getbyid?id=' + id;
-  };
-
-  // Food
-  static FoodGetAll = AppUris.RootUri + 'api/food/getall';
-  static FoodCreate = AppUris.RootUri + 'api/food/create';
-  static FoodUpdate = AppUris.RootUri + 'api/food/update';
-  static FoodDelete = AppUris.RootUri + 'api/food/delete';
-  static FoodSearch = AppUris.RootUri + 'api/food/search';
-  static FoodGetById = (id: number) => {
-    return AppUris.RootUri + 'api/food/getbyid?id=' + id;
-  };
-  static SearchFood = (request: any) => {
-    return AppUris.RootUri
-      + 'api/food/searchfood?value='
-      + request.value + '&maxresults='
-      + request.maxResults + '&pagenumber='
-      + request.pageNumber;
-  };
-  static SearchFoodDetails = (id: number) => {
-    return AppUris.RootUri + 'api/food/searchfooddetails?id=' + id;
-  };
+  }
 
   // Identity
   static UserAuthenticate = AppUris.RootUri + 'api/account/authenticate';
@@ -48,7 +28,7 @@ export class AppUris {
   static UserUploadImage = AppUris.RootUri + 'api/account/setuserimage';
   static UserDownloadImage = (id: string) => {
     return AppUris.RootUri + 'api/account/getuserimagestring?id=' + id;
-  };
+  }
   static UserGeById = (id: string) => {
     return AppUris.RootUri + 'api/account/getbyid?id=' + id;
   }
