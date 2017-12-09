cb275f14cf7263c195713f8620e37cfebcae1112
George Vasilchenko <george.vasilchenko@gmail.com>
2017-12-09 23:18:50 +0100

    NBUD-9: Set prod mode.

--- src/app/globals/app-uris.ts
+++ src/app/globals/app-uris.ts
@@ -1,5 +1,5 @@
 export class AppUris {
-  static IsDev = true;
+  static IsDev = false;
   static ProdUri = 'http://nutrition-buddy-api.azurewebsites.net/';
   static DevUri = 'http://localhost:5000/';
   static RootUri = AppUris.IsDev ? AppUris.DevUri : AppUris.ProdUri;
